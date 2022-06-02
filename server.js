58ecee9d745ffc8bde05a52ba7640460057b2c5c
Emily Purser <83991483+emily-purser@users.noreply.github.com>
2022-06-02 12:29:13 -0600

    Got Auth0 on localhost

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connection');
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    issuerBaseURL: process.env.ISSUER_BASE_URL
+  };
 
 app.use(bodyParser.json())
 .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 })
-.use('/', require('./routes'));
+// .use('/', require('./routes'));
+.use(auth(config));
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+})
+
+app.use('/home', requiresAuth(), require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
     if (err) {
@@ -20,3 +42,11 @@ mongodb.initDb((err, mongodb) => {
         console.log(`Connect to DB and listening on port ${port}`);
     }
 });
+
+
+
+
+// // auth router attaches /login, /logout, and /callback routes to the baseURL
+// app.use(auth(config));
+
+
